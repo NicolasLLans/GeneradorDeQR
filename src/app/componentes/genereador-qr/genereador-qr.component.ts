73d676734fc184d89cd4ea82f198afad106f1448
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-25 13:03:09 -0300

    generador de QR terminado

--- src/app/componentes/genereador-qr/genereador-qr.component.ts
+++ src/app/componentes/genereador-qr/genereador-qr.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenereadorQrComponent implements OnInit {
 
+  image!: HTMLImageElement | null;
+  input!: HTMLInputElement | null;
+  button!: HTMLButtonElement | null;
+  api = 'https://api.qrserver.com/v1/';
+  api2 = 'create-qr-code/?size=150x150&data=';
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.image = document.querySelector('img');
+    this.input = document.querySelector('input');
+    this.button = document.querySelector('button');
 
+    this.button!.addEventListener('click', () => {
+      this.image!.src = `${this.api}${this.api2}${this.input?.value}`;
+    });
+  }
 }
