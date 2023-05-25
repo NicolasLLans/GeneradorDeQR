739266f0de90807a655b97cc5baeaa73a27f710e
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-25 13:27:10 -0300

    mejora en el diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenereadorQrComponent } from './componentes/genereador-qr/genereador-qr.component';
+import { LogoComponent } from './componentes/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GenereadorQrComponent
+    GenereadorQrComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
